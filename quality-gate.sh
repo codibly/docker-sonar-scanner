1128d3420059d25b1988dc9ef75d987b9fe43489
Andrzej Piszczek <andrzej.piszczek@codibly.com>
2019-01-16 13:06:48 +0100

    init

---
+++ quality-gate.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# get user token
+token=$(cat sonar-project.properties | sed 's/sonar.login=\(.*\)/\1/;t;d')
+
+get() {
+# in url pass additional auth - ..//password@:
+    url=$(echo $1 | sed "s/\(https\?:\/\/\)\(.*\)/\1$token:@\2/")
+    wget -qO- -c $url || exit 1
+}
+
+serverUrl=$(cat .scannerwork/report-task.txt | sed 's/serverUrl=\(.*\)/\1/;t;d')
+ceTaskUrl=$(cat .scannerwork/report-task.txt | sed 's/ceTaskUrl=\(.*\)/\1/;t;d')
+
+while true; do
+    analysisId=$(get $ceTaskUrl | sed 's/.*analysisId":"\(.*\?\)","status.*}}/\1/')
+
+#   check if anylysis is completed
+    if echo $analysisId | grep -q status; then
+        echo "Waiting for analysis complete..."
+    else
+        break;
+    fi
+# wait until sonar complete report
+    sleep 3
+done
+
+# get status
+status=$(get $serverUrl/api/qualitygates/project_status?analysisId=$analysisId | sed 's/.*"status":"\(.*\)","conditions".*/\1/')
+
+if [ $status != "OK" ]; then
+    echo "Quality gate not passed"
+    exit 1;
+fi;
+
+echo "Quality gate: Passed"
